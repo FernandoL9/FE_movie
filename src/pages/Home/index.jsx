bdb27a05d51c244150c3f8e8f4135fba91ddc3c0
fernando <fernando.lemos@hotmail.com.br>
2022-12-24 12:45:33 -0300

    update home.jsx  v2

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,6 @@
 import {Container, Content, Add} from './styles'
 import { useState, useEffect} from 'react'
+import { api } from '../../services/api'
 
 import { FiPlus } from 'react-icons/Fi'
 
@@ -12,14 +13,19 @@ import {Notes} from '../../components/Notes'
 
 
 export function Home() {
-  const [tags, setTags] = useState([])
+  
+  const { selectedTags, setSelectedTags } = useState([])
+  const { search, setSearch } = useState("")
+  const [notes, setNotes] = useState([])
 
   useEffect(() => {
-    async function fetchtags(){
-      const response = await api.get("/tags")
-      setTags(response.data)
+    async function fetchNotes(){
+      const response = await api.get(`/notes?title=${search}&tags=${selectedTags}`)
+      setNotes(response.data)
     }
-  },[])
+    fetchNotes()
+  },[selectedTags, search])
+
   return (
     <Container>
       <Header/>
@@ -33,7 +39,17 @@ export function Home() {
           </div>
           <Section>       
             <div className="info">
-                <Notes data={{
+
+              {
+                notes.map(note => (
+                  <Notes
+                    key={String(note.id)}
+                    data={note}
+                  />
+                ))
+              }
+
+                {/* <Notes data={{
                   title: "Interstellar",
                   star: [
                     {id: "1", isActive:"false"},
@@ -43,20 +59,7 @@ export function Home() {
                     {id: "2", isActive:"true"},
                   ],
                   details: "Pragas nas colheitas fizeram a civilização humana regredir para uma sociedade agrária em futuro de data desconhecida. Cooper, ex-piloto da NASA, tem uma fazenda com sua família. Murphy, a filha de dez anos de Cooper, acredita que seu quarto está assombrado por um fantasma que tenta se..."
-                }}/>
-            </div>
-            <div className="info">
-                <Notes data={{
-                  title: "Interstellar",
-                  star: [
-                    {id: "1", isActive:"false"},
-                    {id: "1", isActive:"false"},
-                    {id: "1", isActive:"false"},
-                    {id: "1", isActive:"false"},
-                    {id: "2", isActive:"true"},
-                  ],
-                  details: "Pragas nas colheitas fizeram a civilização humana regredir para uma sociedade agrária em futuro de data desconhecida. Cooper, ex-piloto da NASA, tem uma fazenda com sua família. Murphy, a filha de dez anos de Cooper, acredita que seu quarto está assombrado por um fantasma que tenta se..."
-                }}/>
+                }}/> */}
             </div>
           </Section>   
         </main>
